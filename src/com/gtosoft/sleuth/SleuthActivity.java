1a0a73b17fa8b922d237bf83fa0f6e1357414bce
Brad Hein <gtosoft@gmail.com>
2011-09-25 14:16:51 -0400

    released to SR for data logging purposes. Logs packets to sdcard/voyager/replaylog

--- src/com/gtosoft/sleuth/SleuthActivity.java
+++ src/com/gtosoft/sleuth/SleuthActivity.java
@@ -18,6 +18,8 @@ import com.gtosoft.libvoyager.session.MonitorSession;
 import com.gtosoft.libvoyager.util.EasyTime;
 import com.gtosoft.libvoyager.util.EventCallback;
 import com.gtosoft.libvoyager.util.GeneralStats;
+import com.gtosoft.libvoyager.util.NetworkStats;
+import com.gtosoft.libvoyager.util.OOBMessageTypes;
 
 public class SleuthActivity extends Activity {
 
@@ -99,8 +101,10 @@ public class SleuthActivity extends Activity {
 		// Either resume the old device or kick off a discovery.
 		if (mBTPeerMAC.length() > 0)
 			setupSession(mBTPeerMAC);
-		else
+		else {
 			maHelper.startDiscovering();
+			msg ("Searching for device...");
+		}
 	}
 
 	/**
@@ -145,10 +149,10 @@ public class SleuthActivity extends Activity {
 		}
 
 		// mStatusBox.setStatusLevel("Connecting to " + deviceMACAddress, 2);
-		hs = new HybridSession(BluetoothAdapter.getDefaultAdapter(),
-				deviceMACAddress, ddb, ecbOOBMessageHandler);
+		hs = new HybridSession(BluetoothAdapter.getDefaultAdapter(),deviceMACAddress, ddb, ecbOOBMessageHandler);
 		// register a method to be called when new data arrives.
 		hs.registerDPArrivedCallback(ecbDPNArrivedHandler);
+
 
 		startDataCollectorLoop();
 
@@ -168,7 +172,7 @@ public class SleuthActivity extends Activity {
 		mtDataCollector = new Thread() {
 			public void run() {
 				int loops = 0;
-				boolean hardwareDetected = false;
+//				boolean hardwareDetected = false;
 				while (mThreadsOn == true) {
 					loops++;
 					mgStats.setStat("loops", "" + loops);
@@ -189,6 +193,11 @@ public class SleuthActivity extends Activity {
 						// moni by the detectSessionAndStartSniffing process...
 						if (hs.getCurrentSessionType() == HybridSession.SESSION_TYPE_MONITOR) {
 							MonitorSession m = hs.getMonitorSession();
+							
+							if (loops % 5 == 0) {
+								dumpStatsToLogfile();
+							}
+							
 							// Show GeneralStats leading up to a full connection
 							// state. After that, display DPNs.
 							if (m != null
@@ -197,7 +206,10 @@ public class SleuthActivity extends Activity {
 											.length() > 0) {
 								dumpAllDPNsToScreen();
 							} else {
-								dumpStatsToScreen();
+								// network not ID'd but we're sniffing. 
+//								if (loops % 10 == 0) dumpStatsToScreen();
+								clearMessages();
+								msg ("monitor stats: \n" + hs.getNetworkStats().getStatsString());
 							}
 						}
 					} else {
@@ -224,41 +236,41 @@ public class SleuthActivity extends Activity {
 		msg(hs.getPIDDecoder().getAllDataPointsAsString());
 	}
 
-	// TODO: Make sure hybridSEssion is using the new obd packet parser class to
-	// decode responses.
-	/**
-	 * Do whatever necessary to detect hardware type and get it into sniff mode.
-	 * We only try to detect the hardware once, if it fails, we return false. So
-	 * if you get a false reading from this method, make another call. repeat as
-	 * necessary.
-	 */
-	private boolean detectSessionAndStartSniffing() {
-		msg("Running session detection...");
-		boolean result = hs.runSessionDetection();
-		msg("Session detection result was " + hs.getCapabilitiesString());
-		if (result == true) {
-			// session detection was successful, move to next phase.
-			msg("Is hardware SWCAN? " + hs.isHardwareSWCAN());
-			msg("Session detection succeeded, Switching to moni session... If possible. ");
-			if (hs.isHardwareSniffable() && hs.isHardwareSWCAN() != true) {
-				msg("Hardware IS sniffable, so switching to moni");
-				hs.setActiveSession(HybridSession.SESSION_TYPE_MONITOR);
-				msg("After switch to moni.");
-			} else {
-				msg("Hardware does not support sniff.");
-				return false;
-			}
-
-			if (hs.isHardwareSWCAN()) {
-				msg("Proceed with command sending now.");
-			}
-		} else {
-			// return value of the session deteciton was false so return false.
-			return false;
-		}
-
-		return true;
-	}
+//	// TODO: Make sure hybridSEssion is using the new obd packet parser class to
+//	// decode responses.
+//	/**
+//	 * Do whatever necessary to detect hardware type and get it into sniff mode.
+//	 * We only try to detect the hardware once, if it fails, we return false. So
+//	 * if you get a false reading from this method, make another call. repeat as
+//	 * necessary.
+//	 */
+//	private boolean detectSessionAndStartSniffing() {
+//		msg("Running session detection...");
+//		boolean result = hs.runSessionDetection();
+//		msg("Session detection result was " + hs.getCapabilitiesString());
+//		if (result == true) {
+//			// session detection was successful, move to next phase.
+//			msg("Is hardware SWCAN? " + hs.isHardwareSWCAN());
+//			msg("Session detection succeeded, Switching to moni session... If possible. ");
+//			if (hs.isHardwareSniffable() && hs.isHardwareSWCAN() != true) {
+//				msg("Hardware IS sniffable, so switching to moni");
+//				hs.setActiveSession(HybridSession.SESSION_TYPE_MONITOR);
+//				msg("After switch to moni.");
+//			} else {
+//				msg("Hardware does not support sniff.");
+//				return false;
+//			}
+//
+//			if (hs.isHardwareSWCAN()) {
+//				msg("Proceed with command sending now.");
+//			}
+//		} else {
+//			// return value of the session deteciton was false so return false.
+//			return false;
+//		}
+//
+//		return true;
+//	}
 
 	private void msg(final String messg) {
 		final String m = getThreadID() + " " + messg;
@@ -268,6 +280,15 @@ public class SleuthActivity extends Activity {
 				tvMain.append(m + "\n");
 			}
 		});
+	}
+	
+	private void clearMessages () {
+		muiHandler.post(new Runnable() {
+			public void run() {
+				tvMain.setText("");
+			}
+		});
+		
 	}
 
 	private String getThreadID() {
@@ -280,33 +301,38 @@ public class SleuthActivity extends Activity {
 	EventCallback ecbOOBMessageHandler = new EventCallback() {
 		@Override
 		public void onOOBDataArrived(String dataName, String dataValue) {
+			
+			// IO state changed, we connected, kick off discovery?
+//			if () 
 
 			if (mThreadsOn != true) {
-				msg("Ignoring OOB message out of scope. Threads are off. "
-						+ dataName + "=" + dataValue);
+				msg("Ignoring OOB message out of scope. Threads are off. " + dataName + "=" + dataValue);
 				return;
 			}
 
 			msg("OOB Data: " + dataName + "=" + dataValue);
 
-			if (dataName
-					.equals(HybridSession.OOBMessageTypes.AUTODETECT_SUMMARY)) {
+			if (dataName.equals(OOBMessageTypes.AUTODETECT_SUMMARY)) {
 				if (hs.isDetectionValid() == true) {
-					msg("Hardware detection completed. String="
-							+ hs.getCapabilitiesString());
+					msg("Hardware detection completed. String=" + hs.getCapabilitiesString());
+					
+					if (hs.isHardwareSniffable() == true) {
+						msg ("Hardware is sniffable - Jumping to monitor mode now!");
+						hs.setActiveSession(HybridSession.SESSION_TYPE_MONITOR);
+						turnOnLogging ();
+					}
 				}
 			}
 
 			// state change?
-			if (dataName.equals(HybridSession.OOBMessageTypes.IO_STATE_CHANGE)) {
+			if (dataName.equals(OOBMessageTypes.IO_STATE_CHANGE)) {
 				int newState = 0;
 				try {
 					newState = Integer.valueOf(dataValue);
 					msg("IO State changed to " + newState);
 					// ioStateChanged(newState);
 				} catch (NumberFormatException e) {
-					msg("ERROR: Could not interpret new state as string: "
-							+ dataValue);
+					msg("ERROR: Could not interpret new state as string: " + dataValue);
 				}
 
 				if (newState > 0) {
@@ -325,9 +351,7 @@ public class SleuthActivity extends Activity {
 			}// end of "if this was a io state change".
 
 			// session state change?
-			if (dataName
-					.equals(HybridSession.OOBMessageTypes.SESSION_STATE_CHANGE)
-					&& hs.getCurrentSessionType() == HybridSession.SESSION_TYPE_MONITOR) {
+			if (dataName.equals(OOBMessageTypes.SESSION_STATE_CHANGE) && hs.getCurrentSessionType() == HybridSession.SESSION_TYPE_MONITOR) {
 				int newState = 0;
 
 				// convert from string to integer.
@@ -342,6 +366,22 @@ public class SleuthActivity extends Activity {
 		}// end of session state change handler.
 	};// end of override.
 
+	
+	private void turnOnLogging () {
+		// Turn on logging and dump stats to it. 
+		msg ("Turning on logging, dumping stats.");
+		NetworkStats ns = hs.getNetworkStats();
+		if (ns == null ) {
+			msg ("No networkstats object defined yet. Unable to collect network stats.");
+			return;
+		}
+		ns.setLogging(true);
+		String stats = getStats().getAllStats();
+		ns.appendLog(stats);
+		msg ("Stats filename = " + ns.getLogFilePath());
+	}
+	
+	
 	EventCallback ecbDPNArrivedHandler = new EventCallback() {
 		@Override
 		public void onDPArrived(String DPN, String sDecodedData,
@@ -357,6 +397,12 @@ public class SleuthActivity extends Activity {
 		return mgStats;
 	}
 
+	
+	private void dumpStatsToLogfile () {
+		final String _stats = getStats().getAllStats();
+		hs.getNetworkStats().appendLog(_stats);
+	}
+	
 	private void dumpStatsToScreen() {
 		final String _stats = getStats().getAllStats();
 		msg(_stats);
@@ -405,6 +451,9 @@ public class SleuthActivity extends Activity {
 	}// end of setOnButtonEventHandlers.
 
 	private boolean doCommand (String cmdName) {
+		msg ("Commands are disabled for now. Data-collection only.");
+		return true;
+/*
 		if (hs.isHardwareSWCAN() != true) {
 			msg("WARANING: hardware may not be SWCAN");
 		}
@@ -422,9 +471,14 @@ public class SleuthActivity extends Activity {
 		hs.getCommandSession().sendCommand(cmdName);
 		
 		return true;
+*/
 	}
 
 	private void doWakeup () {
+		msg ("Wake-up DISABLED for now...");
+		
+		/*		
+		
 		if (hs.getCurrentSessionType() != HybridSession.SESSION_TYPE_COMMAND) {
 			msg("Switching to command session (");
 			hs.setActiveSession(HybridSession.SESSION_TYPE_COMMAND);
@@ -433,6 +487,8 @@ public class SleuthActivity extends Activity {
 		msg ("Sending wakeup...");
 		hs.getCommandSession().wakeUpAllNetworks();
 		msg ("Wakeup has been sent. ");
+		
+		*/
 	}
 	
 	
